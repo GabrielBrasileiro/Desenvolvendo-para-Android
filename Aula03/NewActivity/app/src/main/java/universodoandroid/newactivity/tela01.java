696cd50b5dea7bef8524b954207c921e16bdf182
Gabriel Brasileiro <gah.lucas@gmail.com>
2017-05-05 01:56:21 -0300

    Aula03

---
+++ Aula03/NewActivity/app/src/main/java/universodoandroid/newactivity/tela01.java
@@ -0,0 +1,13 @@
+package universodoandroid.newactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tela01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela01);
+    }
+}
